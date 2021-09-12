2651199550979808ff65f4fc6c199c7103759142
Camilo Groberio <camilo.groberio@gmail.com>
2021-09-12 20:30:54 -0300

    DataTable integration

--- frontend/src/components/DataTable/index.tsx
+++ frontend/src/components/DataTable/index.tsx
@@ -1,4 +1,27 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { SalePage } from "types/sale";
+import { formatLocalDate } from "utils/format";
+import { BASE_URL } from "utils/requests";
+
 const DataTable = () => {
+
+    const [page, setPage] = useState<SalePage>({
+        first: true,
+        last: true,
+        number: 0,
+        totalElements: 0,
+        totalPages: 0
+     } )
+
+    useEffect(() => {
+        axios.get(`${BASE_URL}/sales?page=0&size=20&sort=date,desc`)
+            .then(response  => {
+                setPage(response.data);
+            });
+    },[]);
+
+
     return (
         <div className="table-responsive">
             <table className="table table-striped table-sm">
@@ -12,13 +35,17 @@ const DataTable = () => {
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>22/04/2021</td>
-                        <td>Barry Allen</td>
-                        <td>34</td>
-                        <td>25</td>
-                        <td>15017.00</td>
-                    </tr>
+                    {page.content?.map(x => (
+                        <tr key={x.id}>
+                            <td>{formatLocalDate(x.date,"dd/MM/yyyy")}</td>
+                            <td>{x.seller.name}</td>
+                            <td>{x.visited}</td>
+                            <td>{x.deals}</td>
+                            <td>{x.amount}</td>
+                        </tr>
+                    ))
+                    }
+
                 </tbody>
             </table>
         </div>
--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -1,3 +1,26 @@
+import { Seller } from "./seller"
+
+export type Sale = {
+    id: number;
+    visited: number;
+    deals: number;
+    amount: number;
+    date: string;
+    seller: Seller;
+}
+
+export type SalePage = {
+    first: boolean;
+    last: boolean;
+    number: number;
+    totalElements: number;
+    totalPages: number;
+    numberOfElements?: number;
+    size?: number;
+    empty?: boolean;
+    content?: Sale[];
+}
+
 export type SaleSum = {
     sellerName: string;
     saleSum: number;
