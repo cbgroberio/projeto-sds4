dcde49a49da4fa7339369a5a1b46574ed322a118
Camilo Groberio <camilo.groberio@gmail.com>
2021-09-12 22:12:47 -0300

    Env var

--- frontend/src/utils/requests.ts
+++ frontend/src/utils/requests.ts
@@ -1,1 +1,2 @@
-export const BASE_URL = 'http://localhost:8080'
+// ?? - se a variavel REACT_APP_BACKEND_URL não estiver definida, usa o valor fixado
+export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? 'http://localhost:8080';
